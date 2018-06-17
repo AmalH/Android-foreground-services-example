c3ce247be0dd53005fd1552bedf11ecb00751758
AmalH <hichri.amal@hotmail.fr>
2018-06-17 21:52:16 +0100

    final

---
+++ app/src/main/java/amalhichri/androidprojects/com/androidforegroundservicesexample/Statics.java
@@ -0,0 +1,18 @@
+package amalhichri.androidprojects.com.androidforegroundservicesexample;
+
+public class Statics {
+
+    public interface ACTION {
+        public static String MAIN_ACTION = "com.truiton.foregroundservice.action.main";
+        public static String PREV_ACTION = "com.truiton.foregroundservice.action.prev";
+        public static String PLAY_ACTION = "com.truiton.foregroundservice.action.play";
+        public static String NEXT_ACTION = "com.truiton.foregroundservice.action.next";
+        public static String STARTFOREGROUND_ACTION = "com.truiton.foregroundservice.action.startforeground";
+        public static String STOPFOREGROUND_ACTION = "com.truiton.foregroundservice.action.stopforeground";
+    }
+
+    public interface NOTIFICATION_ID {
+        public static int FOREGROUND_SERVICE = 101;
+    }
+
+}
